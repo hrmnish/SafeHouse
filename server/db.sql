e6899808a970eaf20d26565f6ba953b7545c809a
Harmanjot Singh <harmanjotsingh33@gmail.com>
2022-02-04 16:38:34 -0800

    commit

--- server/db.sql
+++ server/db.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE safehouse;
-
 CREATE TABLE users(
   user_id uuid DEFAULT uuid_generate_v4(),    --To make this work download extension in psql "create extention if not exists "uuid-oops""
   user_name VARCHAR(255) NOT NULL,
