7f445d9ad4babce9f95cb70b2e3281ade6855de8
Harmanjot Signh <harmanjotsingh33@gmail.com>
2022-02-15 12:33:42 -0800

    made dashboard for backend
    has functions to support messaging system

--- server/db.sql
+++ server/db.sql
@@ -1,10 +1,32 @@
 
 CREATE extension IF NOT EXISTS "uuid-ossp"
 
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
   user_id uuid DEFAULT uuid_generate_v4(),
   user_name VARCHAR(255) NOT NULL,
   user_email VARCHAR(255) NOT NULL UNIQUE,
   user_password VARCHAR(255) NOT NULL,
   PRIMARY KEY(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS letters(
+  letter_id uuid DEFAULT uuid_generate_v4(),
+  sender_id uuid NOT NULL,
+  letter VARCHAR() -- add in later
+  responses INT DEFAULT 0,
+  recievable BOOLEAN DEFAULT TRUE,
+  send_date DATETIME DEFAULT GETDATE(), 
+  PRIMARY KEY(letter_id),
+  FOREIGN KEY sender_id REFERENCES users(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS responses(
+  response_id uuid DEFAULT uuid_generate_v4(),
+  letter_id uuid NOT NULL,
+  sender_id uuid NOT NULL,
+  response VARCHAR(), -- FIX LATER
+  send_date DATETIME DEFAULT GETDATE(), 
+  PRIMARY KEY(message_id),
+  FOREIGN KEY letter_id REFERENCES letters(letter_id),
+  FOREIGN KEY reciver_id REFERENCES users(user_id)
 );
--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -1,10 +1,69 @@
 const router = require("express").Router();
 const authorize = require("../middleware/authorization");
 const pool = require("../db");
+var cache = require('memory-cache');
+var counter = 0;
 
-router.get("/", authorize, async(req,res) => {
+router.get("/sendletter", authorize, async(req,res) => {
     try {
-        const user = await pool.query("SELECT user_name FROM users WHERE user_id = $1", [req.user])
+        const {letter} = req.body;
+
+        const insert = await pool.query("INSERT INTO letters (sender_id, letter) VALUES ($1, $2)", [req.user, letter]);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");     
+    }
+})
+
+router.get("/getletter", authorize, async(req,res) => {
+    try {
+
+        if (cache.size() < 10) {
+            const letters = await pool.query("SELECT * FROM letters ORDER BY responses DESC WHERE responses <= 10 AND sender_id <> $1", [req.user]);
+            for (i = 0; i < letters.rows.length; i++) {
+                cache.put(letters.rows[i].letter_id, letters.rows[i].letter, 300);
+            }
+        }
+        
+        var keys = cache.keys();
+        if (counter != 10) {
+            counter++;
+            return res.json({ keys,[counter] : cache.get(keys[counter]) });
+        }
+
+        if (counter == 10) { counter = 0; }
+
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");     
+    }
+})
+
+router.get("/sendresponse", authorize, async(req,res) => {
+    try {
+        const {letter_id, letter} = req.body;
+
+        const insert = await pool.query("INSERT INTO responses (letter_id, sender_id, response) VALUES ($1, $2, $3)", [letter_id, req.user, letter]);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Server Error");     
+    }
+})
+
+router.get("/getresponse", authorize, async(req,res) => {
+    try {
+        const letter_id = req.body;
+
+        const response = await pool.query("SELECT response FROM responses WHERE letter_id = $1", [letter_id]);
+
+        if (response.rows.length != 0) {
+            return res.json(response.rows[0])
+        } else {
+            return res.status(400).json("Response not found");
+        }
 
     } catch (error) {
         console.error(error.message);
